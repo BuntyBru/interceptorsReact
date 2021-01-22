0dd87d54405524b5cb1d6c96309971ac80b5ff37
BuntyBru <buntybru@gmail.com>
2021-01-22 15:01:45 +0530

    added react router and axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"> hello</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            hey
+          </Route>
+          <Route path="/login">Login</Route>
+          <Route path="/dashboard">dashboard</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
